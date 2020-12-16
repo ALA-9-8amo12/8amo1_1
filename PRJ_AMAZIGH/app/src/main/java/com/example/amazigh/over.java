230575817bb3d4c620142221bbbdf8cae0c77e7b
Anisha2002 <45850205+Anisha2002@users.noreply.github.com>
2020-12-16 12:11:41 +0100

    Applied changes to categorie
    Applied changes to the style of categorie screen

---
+++ PRJ_AMAZIGH/app/src/main/java/com/example/amazigh/over.java
@@ -0,0 +1,14 @@
+package com.example.amazigh;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class over extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_over);
+    }
+}
